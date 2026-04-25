237838e6807b266b183a1a3d5687053e24a388ad
OniKiely <Oisinkiely200@gmail.com>
2026-04-25 15:32:33 -0700

    Added the basic UI for the main popup menu. still needs to be styled also progress bar is not finished

---
+++ popup.js
@@ -0,0 +1,8 @@
+// LABELS
+const currentTime = document.getElementById("currentTime");
+const timeLeft = document.getElementById("timeLeft");
+
+// BUTTONS
+const startBreakButton = document.getElementById("startBreakButton");
+const add15MinsButton = document.getElementById("add15MinsButton");
+const collectionButton = document.getElementById("collectionButton");
